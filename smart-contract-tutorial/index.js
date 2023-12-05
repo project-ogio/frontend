f7118c2ad0891b6eb0ad37ff53f80009c9cddf93
oyintoboss <isreal.oyinloye11@gmail.com>
2023-12-05 22:55:39 +0000

    The name AJoo was changed to OGIO

---
+++ smart-contract-tutorial/index.js
@@ -0,0 +1,17 @@
+const { Web3 } = require('web3'); //  web3.js has native ESM builds and (`import Web3 from 'web3'`)
+
+// Set up a connection to the Ganache network
+// const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+// HTTP://127.0.0.1:7545
+
+const web3 = new Web3(new Web3.providers.HttpProvider('HTTP://127.0.0.1:7545'));
+
+// Log the current block number to the console
+web3.eth
+	.getBlockNumber()
+	.then(result => {
+		console.log('Current block number: ' + result);
+	})
+	.catch(error => {
+		console.error(error);
+	});
